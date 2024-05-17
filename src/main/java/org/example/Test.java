2e51b10c790a14e31ca3bab357cc22bb31f1c57b
Akash-Tandale001 <ak.tandale123@gmail.com>
2024-05-18 00:36:48 +0530

    minor changes

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -11,11 +11,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Test {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Jatin\\Desktop\\Chrome Driver\\chromedriver-win64\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","D:\\Desktop\\STQA\\chromedriverJatin.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--incognito");
 		WebDriver driver = new ChromeDriver();
-		
+
 		driver.manage().window().maximize();
 		driver.get("https://www.linkedin.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(12));
@@ -28,34 +28,38 @@ public class Test {
         locationInput.sendKeys("Pune");
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(12));
         driver.findElement(By.xpath("//input[contains(@value,'jobs-search-bar_search-submit')]/following-sibling::button")).click();
-        
+
         try {
             Thread.sleep(7000); // Add a brief pause to ensure search results are loaded
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        
+
         List<WebElement> allJobs = driver.findElements(By.xpath("//ul[@class='jobs-search__results-list']/li/div/a"));
     	allJobs.get(0).click();
-    	
-//    	
-    	 
-    	 allJobs.get(2).click();
+
+        try {
+            Thread.sleep(7000); // Add a brief pause to ensure search results are loaded
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    	 allJobs.get(1).click();
     	 WebElement companyName = driver.findElement(By.xpath("//a[contains(@data-tracking-control-name,'public_jobs_topcard-org')]"));
-    	 System.out.println(companyName.getText());
-    	 
+    	 System.out.println("Company Name: "+companyName.getText());
+
     	 try {
            Thread.sleep(7000); // Add a brief pause to ensure search results are loaded
        } catch (InterruptedException e) {
            e.printStackTrace();
        }
-    	 
-    	 WebElement showMore = driver.findElement(By.xpath("//button[contains(@data-tracking-control-name,'public_jobs_show-more-html-btn')]"));
+
+    	 WebElement showMore = driver.findElement(By.xpath("/html/body/div[1]/div/section/div[2]/div/section[1]/div/div/section/button[1]"));
          showMore.click();
-         
+
          WebElement jobDescription = driver.findElement(By.xpath("/html/body/div[1]/div/section/div[2]/div/section[1]/div/div/section/div"));
          System.out.println("Job Description :" + jobDescription.getText());
-         
+
          driver.quit();
 	}
 }
